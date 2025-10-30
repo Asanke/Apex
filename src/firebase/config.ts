3bd4f345bbfb2c3e71ab23ffe794cd7a5331d257
Asanke Raynayake <asanke1@gmail.com>
2025-10-30 03:44:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1417219424-43dad",
+  "appId": "1:316844638927:web:3c87dded33f8196d7d2dba",
+  "apiKey": "AIzaSyDfdafXnvowDJdQcHgIKY6k1LrDORLp3WY",
+  "authDomain": "studio-1417219424-43dad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "316844638927"
+};
